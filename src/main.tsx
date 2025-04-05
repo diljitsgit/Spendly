9429d6529b7ffc97b6925bef72125eeb8fd53e07
diljitsgit <diljits1776@gmail.com>
2025-04-06 03:09:56 +0530

    Hopefully final Deployment change

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
-import Dashboard from "./Dashboard";
+import App from "./App.tsx";
+import Dashboard from "./Dashboard.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/Spendly",
+        element: <App />,
+        // errorElement: <ErrorPage />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+        // errorElement: <ErrorPage />,
+    },
+]);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </HashRouter>
+        <RouterProvider router={router} />
     </StrictMode>
 );
