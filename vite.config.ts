e1219b447652a0395288aaad3944ec9af1c39d07
diljitsgit <diljits1776@gmail.com>
2025-04-06 01:13:49 +0530

    Deployment Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    base: "/",
+    plugins: [react()],
+    base: "/graphatory/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        target: "ES2022",
+    },
 });
