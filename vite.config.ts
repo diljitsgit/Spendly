f2e459b00e5ee6fe9a3be6c9ff67cf9832fd36ef
diljitsgit <diljits1776@gmail.com>
2025-04-06 01:22:29 +0530

    Deployment Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/Spendly/",
+    base: "/Spendly",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        target: "ES2022",
+    },
 });
