39aed65b87b4e8b98d8e76b1f7eff5ebde7680dc
diljitsgit <diljits1776@gmail.com>
2025-04-06 07:59:30 +0530

    Hopefully final Deployment change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/Spendly",
+    base: "/Spendly/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
