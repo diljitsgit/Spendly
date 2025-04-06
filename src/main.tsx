623338a51ed31f7cf075c01dfd67b9afa1693be7
diljitsgit <diljits1776@gmail.com>
2025-04-06 08:16:17 +0530

    Hopefully final Deployment change

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,46 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import App from "./App.tsx";
+// import Dashboard from "./Dashboard.tsx";
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/Spendly",
+//         element: <App />,
+//         // errorElement: <ErrorPage />,
+//     },
+//     {
+//         path: "/Spendly/dashboard",
+//         element: <Dashboard />,
+//         // errorElement: <ErrorPage />,
+//     },
+// ]);
+
+// createRoot(document.getElementById("root")!).render(
+//     <StrictMode>
+//         <RouterProvider router={router} />
+//     </StrictMode>
+// );
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
-import Dashboard from "./Dashboard.tsx";
+import App from "./App";
+import Dashboard from "./Dashboard";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
-        path: "/",
+        path: "/Spendly",
         element: <App />,
-        // errorElement: <ErrorPage />,
     },
     {
-        path: "/dashboard",
+        path: "/Spendly/dashboard",
         element: <Dashboard />,
-        // errorElement: <ErrorPage />,
     },
 ]);
 
